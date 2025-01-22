4ef0f2ee44abadd8f532aa4273780c38ad89e197
smyrzash <sanzhar_myrzash@mail.ru>
2025-01-23 03:15:56 +0500

    environment variable port added

--- alice-skill/cmd/skill/flags.go
+++ alice-skill/cmd/skill/flags.go
@@ -1,6 +1,9 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 // неэкспортированная переменная flagRunAddr содержит адрес и порт для запуска сервера
 var flagRunAddr string
@@ -8,4 +11,8 @@ var flagRunAddr string
 func parseFlags() {
 	flag.StringVar(&flagRunAddr, "addr", ":8080", "address and port to run the server")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("RUN_ADDR"); envRunAddr != "" {
+		flagRunAddr = envRunAddr
+	}
 }
